3397041f528aac8bc296ac0099b69283281d82d3
Jasper Woudenberg <mail@jasperwoudenberg.com>
2024-07-14 13:01:41 +0200

    Sort launch options by name length

--- src/desktopapps.zig
+++ src/desktopapps.zig
@@ -4,20 +4,30 @@ const LaunchOption = @import("./launch_option.zig").LaunchOption;
 pub fn options(allocator: std.mem.Allocator) ![]const LaunchOption {
     const xdg_data_dirs_string = try std.process.getEnvVarOwned(allocator, "XDG_DATA_DIRS");
     var xdg_data_dirs = std.mem.splitScalar(u8, xdg_data_dirs_string, ':');
-    var result = std.ArrayList(LaunchOption).init(allocator);
+    var launch_options = std.ArrayList(LaunchOption).init(allocator);
     while (xdg_data_dirs.next()) |dir_path| {
         var dir = std.fs.openDirAbsolute(dir_path, .{ .iterate = true }) catch continue;
         defer dir.close();
         var walker = try dir.walk(allocator);
         defer walker.deinit();
         while (try walker.next()) |desktop_file_path| {
-            const option = LaunchOption{
-                .display_name = desktop_file_path.path,
-                .search_string = "search",
-                .launch_action = .{ .exec = "exec" },
-            };
-            try result.append(option);
+            const option = parseDesktopAppFile(desktop_file_path.path);
+            try launch_options.append(option);
         }
     }
-    return result.toOwnedSlice();
+    const result = try launch_options.toOwnedSlice();
+    std.sort.block(LaunchOption, result, {}, cmpByNameLength);
+    return result;
+}
+
+fn cmpByNameLength(_: void, a: LaunchOption, b: LaunchOption) bool {
+    return a.display_name.len < b.display_name.len;
+}
+
+fn parseDesktopAppFile(path: []const u8) LaunchOption {
+    return LaunchOption{
+        .display_name = path,
+        .search_string = "search",
+        .launch_action = .{ .exec = "exec" },
+    };
 }
