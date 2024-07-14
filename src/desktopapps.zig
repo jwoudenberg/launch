0b6bed803532402b459bafef09cf62dd9486eaaf
Jasper Woudenberg <mail@jasperwoudenberg.com>
2024-07-14 13:58:57 +0200

    Iterate over .desktop files

--- src/desktopapps.zig
+++ src/desktopapps.zig
@@ -6,12 +6,22 @@ pub fn options(allocator: std.mem.Allocator) ![]const LaunchOption {
     var xdg_data_dirs = std.mem.splitScalar(u8, xdg_data_dirs_string, ':');
     var launch_options = std.ArrayList(LaunchOption).init(allocator);
     while (xdg_data_dirs.next()) |dir_path| {
-        var dir = std.fs.openDirAbsolute(dir_path, .{ .iterate = true }) catch continue;
+        const apps_path = try std.fs.path.join(allocator, &[_][]const u8{ dir_path, "applications" });
+        defer allocator.free(apps_path);
+
+        var dir = std.fs.openDirAbsolute(apps_path, .{ .iterate = true }) catch continue;
         defer dir.close();
+
         var walker = try dir.walk(allocator);
         defer walker.deinit();
+
         while (try walker.next()) |desktop_file_path| {
-            const option = parseDesktopAppFile(desktop_file_path.path);
+            if (!std.mem.endsWith(u8, desktop_file_path.path, ".desktop")) {
+                continue;
+            }
+            const path = try std.fs.path.join(allocator, &[_][]const u8{ apps_path, desktop_file_path.path });
+            defer allocator.free(path);
+            const option = try parseDesktopAppFile(allocator, path);
             try launch_options.append(option);
         }
     }
@@ -24,10 +34,26 @@ fn cmpByNameLength(_: void, a: LaunchOption, b: LaunchOption) bool {
     return a.display_name.len < b.display_name.len;
 }
 
-fn parseDesktopAppFile(path: []const u8) LaunchOption {
+fn parseDesktopAppFile(allocator: std.mem.Allocator, path: []const u8) !LaunchOption {
+    std.debug.print("{s}\n", .{path}); // TODO: remove this
+    var file = try std.fs.openFileAbsolute(path, .{});
+    defer file.close();
+
+    var display_name: []const u8 = undefined;
+    var search_string: []const u8 = undefined;
+    var exec: []const u8 = undefined;
+
+    var reader = file.reader();
+    var buf: [1024]u8 = undefined;
+    while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        display_name = try allocator.dupe(u8, "name");
+        search_string = try std.ascii.allocLowerString(allocator, display_name);
+        exec = line;
+    }
+
     return LaunchOption{
-        .display_name = path,
-        .search_string = "search",
-        .launch_action = .{ .exec = "exec" },
+        .display_name = display_name,
+        .search_string = search_string,
+        .launch_action = .{ .exec = exec },
     };
 }
